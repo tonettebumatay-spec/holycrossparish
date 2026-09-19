ce35bd3832c22be6816415579113089bd721c3ff
Rommel Rayos <rayosrommel45@gmail.com>
2026-09-19 14:19:56 +0800

    Fix Appointment model fillable to include new columns

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,12 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
+    protected $table = 'appointments';
+
     protected $fillable = [
-        'user_name', 
-        'service_type', 
-        'appointment_date', 
-        'contact_number', 
-        'details', 
-        'status'
+        'user_name',
+        'service_type',
+        'appointment_date',
+        'appointment_time',
+        'contact_number',
+        'details',
+        'status',
+        'user_id',
+        'email',
+        'cancellation_reason',
+        'is_locked',
+    ];
+
+    protected $casts = [
+        'appointment_date' => 'date',
+        'is_locked'        => 'boolean',
     ];
 }
